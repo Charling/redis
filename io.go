77d0a225d09ce2dc5e2cf7a6de6941eb85d737b7
Charling <56430114@qq.com>
2019-08-22 16:20:16 +0800

    add io.go

---
+++ io.go
@@ -0,0 +1,38 @@
+package redis
+
+import (
+	"github.com/gomodule/redigo/redis"
+	"time"
+)
+
+var (
+	conn *redis.Pool
+)
+
+func RedisPool(host string) *redis.Pool {
+	conn = &redis.Pool{
+        //最大空闲
+        MaxIdle: 100,
+        //最大活动量
+        MaxActive: 30,
+        //空闲超时
+        IdleTimeout: 60 * time.Second,
+        Dial: func() (redis.Conn, error) {
+            c, err := redis.Dial("tcp", host)
+            if err != nil {
+                return nil, err
+            }
+            return c, err
+        },
+        TestOnBorrow: func(c redis.Conn, t time.Time) error {
+            _, err := c.Do("PING")
+            return err
+        },
+	}
+	
+	return conn
+}
+
+func Get() *redis.Pool {
+	return conn
+}
